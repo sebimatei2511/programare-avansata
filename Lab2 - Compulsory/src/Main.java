5268f9f89f5c6412f055528ed542c97761140aac
sebimatei2511 <79249972+sebimatei2511@users.noreply.github.com>
2022-03-06 20:15:42 +0200

    Update Main.java

--- Lab2 - Compulsory/src/Main.java
+++ Lab2 - Compulsory/src/Main.java
@@ -8,25 +8,36 @@ public class Main {
 
         Event C1 = new Event("C1",100,8,10);
         events.add(C1);
-        System.out.println(C1.getName()+"(size="+C1.getSize()+", start="+C1.getStart()+", end="+C1.getEnd()+")");
+
         Event C2 = new Event("C2",100,10,12);
         events.add(C2);
-        System.out.println(C2.getName()+"(size="+C2.getSize()+", start="+C2.getStart()+", end="+C2.getEnd()+")");
+
         Event L1 = new Event("L1",30,8,10);
         events.add(L1);
-        System.out.println(L1.getName()+"(size="+L1.getSize()+", start="+L1.getStart()+", end="+L1.getEnd()+")");
+
         Event L2 = new Event("L2",30,8,10);
         events.add(L2);
-        System.out.println(L2.getName()+"(size="+L2.getSize()+", start="+L2.getStart()+", end="+L2.getEnd()+")");
 
-        Room room401 = new Room("401",30, Room.type.lab);
+        Event L3 = new Event("L3",30,10,12);
+        events.add(L3);
+
+        Room room401 = new Room("401",30, RoomType.lab);
         rooms.add(room401);
-        Room room403 = new Room("403",30, Room.type.lab);
+
+        Room room403 = new Room("403",30, RoomType.lab);
         rooms.add(room403);
-        Room room405 = new Room("405",30, Room.type.lab);
+
+        Room room405 = new Room("405",30, RoomType.lab);
         rooms.add(room405);
-        Room room309 = new Room("309",100, Room.type.lectureHall);
+
+        Room room309 = new Room("309",100, RoomType.lectureHall);
         rooms.add(room309);
 
+        for ( int i = 0; i < events.size(); i++ )
+            System.out.println(events.get(i).toString());
+        System.out.println();
+        for ( int i = 0; i < rooms.size(); i++ )
+            System.out.println(rooms.get(i).toString());
     }
+
 }
