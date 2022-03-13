0f39bfa608491857dace662f0c3f3cd11284806e
sebimatei2511 <79249972+sebimatei2511@users.noreply.github.com>
2022-03-13 19:07:55 +0200

    Update Node.java

--- Lab3 - Compulsory/src/Node.java
+++ Lab3 - Compulsory/src/Node.java
@@ -1,6 +1,18 @@
-public abstract class Node implements Identifiable, Storage{
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class Node implements Identifiable, Storage, Comparable<Node>{
 
     public String name;
+    private Map<Node, Integer> cost = new HashMap<>();
+
+    public Map<Node, Integer> getCost() {
+        return cost;
+    }
+
+    public void setCost(Map<Node, Integer> cost) {
+        this.cost = cost;
+    }
 
     public Node(String name) { this.name = name; }
 
@@ -8,8 +20,21 @@ public abstract class Node implements Identifiable, Storage{
 
     public void setName(String name) { this.name = name; }
 
+    public void setCost(Node node, int value) {
+        cost.put(node, value);
+    }
+
     @Override
     public String toString() {
         return super.toString();
+    }
+
+    @Override
+    public int compareTo(Node other) {
+        if (this.getName()!=null)
+            return this.getName().compareTo(other.getName());
+        else
+            return 0;
+
     }
 }
