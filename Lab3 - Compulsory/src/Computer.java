0901be1a11f0351327aec1613ee807b0a7aa67bc
sebimatei2511 <79249972+sebimatei2511@users.noreply.github.com>
2022-03-13 19:06:38 +0200

    Update Computer.java

--- Lab3 - Compulsory/src/Computer.java
+++ Lab3 - Compulsory/src/Computer.java
@@ -1,5 +1,27 @@
 public class Computer extends Node implements Identifiable, Storage{
+    private String address;
+    private int storageCapacity;
+
     public Computer(String name) {
         super(name);
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+    @Override
+    public String getAddress() {
+        return null;
+    }
+
+    @Override
+    public int getStorageCapacity() {
+        return storageCapacity;
+    }
+
+    @Override
+    public int compareTo(Node o) {
+        return this.getName().compareTo(o.getName());
     }
 }
