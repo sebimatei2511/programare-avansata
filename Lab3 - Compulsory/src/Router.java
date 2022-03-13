d5672f654e48ff84e6fae2285d91fe900b196a7c
sebimatei2511 <79249972+sebimatei2511@users.noreply.github.com>
2022-03-13 19:08:11 +0200

    Update Router.java

--- Lab3 - Compulsory/src/Router.java
+++ Lab3 - Compulsory/src/Router.java
@@ -2,4 +2,19 @@ public class Router extends Node implements Identifiable, Storage{
     public Router(String name) {
         super(name);
     }
+
+    @Override
+    public String getAddress() {
+        return null;
+    }
+
+    @Override
+    public int getStorageCapacity() {
+        return 0;
+    }
+
+    @Override
+    public int compareTo(Node o) {
+        return this.getName().compareTo(o.getName());
+    }
 }
