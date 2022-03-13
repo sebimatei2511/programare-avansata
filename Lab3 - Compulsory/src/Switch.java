5f6402adc71c8c1fc933812a8b87f187c1c3e435
sebimatei2511 <79249972+sebimatei2511@users.noreply.github.com>
2022-03-13 19:08:40 +0200

    Update Switch.java

--- Lab3 - Compulsory/src/Switch.java
+++ Lab3 - Compulsory/src/Switch.java
@@ -2,4 +2,19 @@ public class Switch extends Node implements Identifiable, Storage{
     public Switch(String name) {
         super(name);
     }
+
+    @Override
+    public String getAddress() {
+        return null;
+    }
+
+    @Override
+    public int getStorageCapacity() {
+        return 0;
+    }
+
+    @Override
+    public int compareTo(Node o) {
+        return this.getName().compareTo(o.getName());
+    }
 }
