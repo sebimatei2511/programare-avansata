8cab7ec132b19fea5004956428a7e4776dcdde52
sebimatei2511 <79249972+sebimatei2511@users.noreply.github.com>
2022-03-14 18:10:03 +0200

    Add files via upload

---
+++ Lab4_Compulsory/src/main/java/Main.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Intersection> intersectionsList = new ArrayList<>();
+
+        Intersection v1 = new Intersection("v1");
+        intersectionsList.add(v1);
+        Intersection v2 = new Intersection("v2");
+        intersectionsList.add(v2);
+        Intersection v3 = new Intersection("v3");
+        intersectionsList.add(v3);
+        Intersection v4 = new Intersection("v4");
+        intersectionsList.add(v4);
+        Intersection v5 = new Intersection("v5");
+        intersectionsList.add(v5);
+        Intersection v6 = new Intersection("v6");
+        intersectionsList.add(v6);
+        Intersection v7 = new Intersection("v7");
+        intersectionsList.add(v7);
+        Intersection v8 = new Intersection("v8");
+        intersectionsList.add(v8);
+        Intersection v9 = new Intersection("v9");
+        intersectionsList.add(v9);
+
+        //Collections.sort(intersectionsList);
+
+    }
+}
---
+++ Lab4_Compulsory/src/main/java/Street.java
@@ -0,0 +1,22 @@
+public class Street{
+    String name;
+    int lenght;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLenght() {
+        return lenght;
+    }
+
+    public void setLenght(int lenght) {
+        this.lenght = lenght;
+    }
+
+
+}
